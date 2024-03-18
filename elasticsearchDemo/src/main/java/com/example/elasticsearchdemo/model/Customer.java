cad94a25941c23d2bd2b036f6245be966682a21e
ThinhLD <leduythinh20001@gmail.com>
2024-03-18 15:29:10 +0700

    Add comment

--- elasticsearchDemo/src/main/java/com/example/elasticsearchdemo/model/Customer.java
+++ elasticsearchDemo/src/main/java/com/example/elasticsearchdemo/model/Customer.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
-@Document(indexName = "demo")
+@Document(indexName = "customer")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
